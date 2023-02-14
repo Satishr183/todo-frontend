eaf4f7cbe6ed5cd8b180134b8f9b0fdf8a6bdc89
Satish <satish_95raj@outlook.com>
2023-02-14 06:47:11 +0530

    Todo 2nd  phase done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,52 @@
 import React, {useState,useEffect} from 'react'
 import axios from "axios"
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+  background: isDragging ? "lightgreen" : "grey",
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250,
+})
 
 function TodoList() {
 
 const [data,setData] = useState([])
+
+
+const onDragEnd = (result) => {
+  if (!result.destination) {
+    return;
+  }
+
+  const reorderedItems = reorder(
+    data,
+    result.source.index,
+    result.destination.index
+  );
+
+  console.log({ reorderedItems });
+  setData(reorderedItems);
+};
 
 useEffect(()=>{
  axios.get('http://localhost:4000/getTodos').then(res=> setData(res.data.data))
@@ -20,21 +63,41 @@ const deletePost = (id) => {
 // console.log(data)
   return (
     <div style={{display:"flex",padding:"20px"}}>
-      
-      <div className='left' style={{width:"50%", border:"2px solid blue",padding:"20px",marginRight:"10px"}}>
-        {data.map((item,index)=>{
-          return (
-            <div style={{width:"35%",margin:"1rem auto",border:"2px solid red",padding:"10px"}} key={index}>
-            <h4>{item.title}</h4>
-            <p>{item.description}</p>
-            <p>{item.option}</p>
-            <button>edit</button>
-            <button onClick={()=> deletePost(item._id)}>delete</button>
-          </div>
-          )
-        })}
-          
-      </div>
+
+<DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {data.map((item, index) => (
+                <Draggable key={item._id} draggableId={item._id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      className="card"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.title}
+                      <button>edit</button>
+                      <button onClick={()=> deletePost(item._id)}>delete</button>
+                    </div>
+                    
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
       <div className='left'style={{width:"50%", border:"2px solid black",padding:"20px",marginLeft:"10px"}}>
           RIGHT   
       </div>
